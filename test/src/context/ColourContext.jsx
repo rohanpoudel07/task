7a42ff7c4936defb570f42fc153ae0cc2ad8867e
Rohan Poudel <rohanpoudel2@gmail.com>
2023-07-11 09:28:14 +0545

    Used localstorage to save the colour state

--- test/src/context/ColourContext.jsx
+++ test/src/context/ColourContext.jsx
@@ -1,8 +1,8 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 const initialState = {
   colour: "inherit",
-  history: []
+  history: [],
 };
 
 export const ColourContext = createContext();
@@ -10,14 +10,25 @@ export const ColourContext = createContext();
 const colourReducer = (state = initialState, action) => {
   switch (action.type) {
     case "change_colour":
-      return { ...state, colour: action.payload, history: [...state.history, action.payload] };
+      const newState = {
+        ...state,
+        colour: action.payload,
+        history: [...state.history, action.payload],
+      };
+      localStorage.setItem("colourState", JSON.stringify(newState));
+      return newState;
     default:
       return state;
   }
 };
 
 export const ColourContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(colourReducer, initialState);
+  const savedState = localStorage.getItem("colourState");
+  const [state, dispatch] = useReducer(colourReducer, savedState ? JSON.parse(savedState) : initialState);
+
+  useEffect(() => {
+    localStorage.setItem("colourState", JSON.stringify(state));
+  }, [state]);
 
   return (
     <ColourContext.Provider value={{ state, dispatch }}>
